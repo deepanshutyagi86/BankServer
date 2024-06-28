5fd59533c4652309852b9a84585e86facbf9acbd
deepanshutyagi86 <deepanshutyagi0784@gmail.com>
2024-06-28 14:39:02 +0530

    init test

--- src/App.java
+++ src/App.java
@@ -1,4 +1,10 @@
 public class App {
+    /**
+     * The main function that prints "Hello, World!" to the console.
+     *
+     * @param  args  an array of command-line arguments
+     * @throws Exception  if an error occurs during execution
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
